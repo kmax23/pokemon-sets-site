b06ccaa708371e4865d3d0f18229936348e4e2e5
Kris Maxey <krismaxey1998@gmail.com>
2025-09-02 13:55:12 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://example.com', // replace with your domain
+  integrations: [mdx(), sitemap()],
+});
